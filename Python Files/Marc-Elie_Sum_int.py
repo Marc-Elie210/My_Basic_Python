3016081080976304dbdac4b75f6f3f3edd790f29
Marc-Elie210 <60478291+Marc-Elie210@users.noreply.github.com>
2021-01-12 09:38:24 -0500

    .

--- Python Files/Marc-Elie_Sum_int.py
+++ Python Files/Marc-Elie_Sum_int.py
@@ -7,9 +7,8 @@ def sum_int(num):
     else:
         for x in range (num+1): # This loop is looping through the range of num+1 ex if num = 3 range is (0,4)
             total += x #computing the total for each x 
-    return f'Here is your sum, {total}.' 
+    return print(f'Here is your sum, {total}.')
 
 
-
-# num = 3# uncomment and enter a num here
+num = 3 # uncomment and enter a num here
 sum_int(num) # calling the function with num in the parameter 
